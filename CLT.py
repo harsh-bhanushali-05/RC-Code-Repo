9d3eafe4acdf00798d7f646431d7b58dad226ba7
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-01-31 16:24:23 +0530

    Add files via upload

--- CLT.py
+++ CLT.py
@@ -5,7 +5,7 @@ def roll():
     return random.randint(1,6)
 def getAllRoll():
     val=[]
-    for i in range(100000):
+    for i in range(10000000):
         val.append(roll())
     return val
 def getMean(list):
@@ -14,7 +14,7 @@ def getMean(list):
 def batchMean():
     allMean = []
     list= getAllRoll()
-    for i in range(0,100000,10):
+    for i in range(0,10000000,10):
         allMean.append(getMean(list[i:i+10]))
         i=i+1000
     return allMean
